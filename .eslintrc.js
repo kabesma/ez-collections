f1f789dca6a7e89defbc580f79aa84c128527c41
akilsagitarius <akil.dcc@gmail.com>
2021-11-02 17:53:54 +0800

    update library with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 13,
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+  'semi':[2, "never"]
+};
--- dist/collect.js
+++ dist/collect.js
@@ -2,46 +2,51 @@
 
 let item = []
 let temp
-let newArr = []
+const newArr = []
 
+/**
+ * Main Function
+ * @param {array} args The arguments enter value.
+ * @return {function} The function call
+ */
 function collect(args) {
-    item = args
-	return {
-        'all' : all,
-        'avg' : avg,
-        'chunk' : chunk,
-        'collapse' : collapse
-    }
+  item = args
+  return {
+    'all': all,
+    'avg': avg,
+    'chunk': chunk,
+    'collapse': collapse,
+  }
 }
 
 const all = () => {
-    return item
+  return item
 }
 
 const avg = (param = null) => {
-    if(param == null){
-        return item.reduce((acc, curr) => acc + curr) / item.length
-    }
+  if (param == null) {
+    return item.reduce((acc, curr) => acc + curr) / item.length
+  }
 
-    temp = 0
-    item.forEach(
-        val => temp = temp + val[param]
-    )
-    return temp / item.length
+  temp = 0
+  item.forEach(
+      (val) => temp = temp + val[param],
+  )
+  return temp / item.length
 }
 
 const chunk = (param) => {
-    while(item.length) newArr.push(item.splice(0,param))
-    return newArr
+  while (item.length) newArr.push(item.splice(0, param))
+  return newArr
 }
 
 const collapse = () => {
-    let result = item.reduce(function(prev, next) {
-        return prev.concat(next);
-    })
-    return collect(result)
+  const result = item.reduce(function(prev, next) {
+    return prev.concat(next)
+  })
+  return collect(result)
 }
 
 module.exports = {
-    collect,
+  collect,
 }
